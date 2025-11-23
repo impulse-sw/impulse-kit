442224849c23b2d75c55ac648bf768b16470402b
Klimenty Titov <aclo.create@gmail.com>
2025-11-24 01:15:19 +0300

    fix(ui): fix attributes redirecting

--- impulse-ui-kit/components/src/avatar.rs
+++ impulse-ui-kit/components/src/avatar.rs
@@ -1,6 +1,7 @@
 #![allow(missing_docs, dead_code)]
 
 use impulse_ui_kit::utils::cn;
+use leptos::attribute_interceptor::AttributeInterceptor;
 use leptos::prelude::*;
 
 #[component]
@@ -22,20 +23,26 @@ pub fn Avatar(#[prop(into, optional)] class: String, children: Children) -> impl
 #[component]
 pub fn AvatarImage(
   #[prop(into, optional)] class: String,
-  #[prop(attrs)] attrs: Vec<leptos::attr::any_attribute::AnyAttribute>,
 ) -> impl IntoView {
   let context = use_context::<AvatarContext>().expect("AvatarImage must be used within Avatar");
-  let attrs = StoredValue::new_local(attrs);
+  let class = StoredValue::new_local(class);
 
   view! {
-    <Show when=move || !context.fallback.get()>
-      <img
-        data-slot="avatar-image"
-        class=cn(&["aspect-square size-full", class.as_str()])
-        on:error=move |_| context.fallback.set(true)
-        {..attrs.get_value()}
-      />
-    </Show>
+    <AttributeInterceptor let:attrs>
+      {
+        let attrs = StoredValue::new_local(attrs);
+        view! {
+          <Show when=move || !context.fallback.get()>
+            <img
+              data-slot="avatar-image"
+              class=cn(&["aspect-square size-full", class.read_value().as_str()])
+              on:error=move |_| context.fallback.set(true)
+              {..attrs.get_value()}
+            />
+          </Show>
+        }
+      }
+    </AttributeInterceptor>
   }
 }
 
